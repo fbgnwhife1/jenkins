565c5e376b981e669fd9d447966377180f996d5a
fbgnwhife1 <70710552+fbgnwhife1@users.noreply.github.com>
2021-06-02 18:06:18 +0900

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 // aaaaaaaaaaaaa111111111111122aaaa11aaaaaaaaaaaaaaa
 int main(){
-  printf("<h1>Hello, hongik! change 18:04</h1>\n");
+  printf("<h1>Hello, hongik! change 18:06</h1>\n");
   return 0;
 }
