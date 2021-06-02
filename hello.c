7b2278ab9ed1501a8c5ae28a267471f3512afc8e
fbgnwhife1 <70710552+fbgnwhife1@users.noreply.github.com>
2021-06-02 16:48:04 +0900

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 // aaaaaaaaaaaaa111111111111122aaaaaaaaaaaaaaaaaaa
 int main(){
-  printf("<h1>Hello, hongik!11122211111111111111</h1>\n");
+  printf("<h1>Hello, hongik!1</h1>\n");
   return 0;
 }
